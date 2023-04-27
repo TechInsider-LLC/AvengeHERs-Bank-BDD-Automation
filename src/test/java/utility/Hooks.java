d704b5815be6a54fd0e685a4d3f470b0808a4663
NargizaD <128551128+NargizaD@users.noreply.github.com>
2023-04-26 21:03:18 -0400

    Selenium

--- src/test/java/utility/Hooks.java
+++ src/test/java/utility/Hooks.java
@@ -1,7 +1,35 @@
 package utility;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
 public class Hooks {
 
 
+    private static WebDriver driver;
 
+    public static WebDriver getDriver(){
+
+        return driver;
+    }
+
+    @Before
+    public void setUp(){
+
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+
+    }
+
+    @After
+    public void tearDown(){
+
+        driver.quit();
+    }
 }
+
