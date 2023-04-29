b50284a62d2ab7417a5687530622f91261515e5a
NargizaD <128551128+NargizaD@users.noreply.github.com>
2023-04-29 14:22:42 -0400

    AV-416

--- src/test/java/features/TransferBetweenUsers.feature
+++ src/test/java/features/TransferBetweenUsers.feature
@@ -3,3 +3,4 @@ Feature: Transfer Between Users
 
   Scenario: User transfers money to another User
     When User transfers money
+    Then Customer should be successfully transfers money
--- src/test/java/stepDefinitions/TransferBetweenUsersPage.java
+++ src/test/java/stepDefinitions/TransferBetweenUsersPage.java
@@ -3,24 +3,21 @@ package stepDefinitions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class TransferBetweenUsersPage {
 
     WebDriver driver;
-
-    @FindBy(css=".header .aside-bar__menu-toggle")
-    WebElement Menu;
-
-    @FindBy(partialLinkText ="TRANSFERS")
-    WebElement Transfer;
+    @FindBy(css =".aside__label main_color transfer")
+    WebElement Transfers;
 
     @FindBy(partialLinkText="Transfer Between Users")
     WebElement TransferBetweenUsers;
 
-    @FindBy(css=".ng-placeholder")
+    @FindBy(css=".ng-value-container")
     WebElement DebitForm;
 
-    @FindBy(css="#a7a3573d7782")
+    @FindBy(css="#ab3747425952")
     WebElement Card;
 
     @FindBy(css="#username")
@@ -35,7 +32,24 @@ public class TransferBetweenUsersPage {
     @FindBy(css="def-btn-success main_color")
     WebElement Continue;
 
+    @FindBy(css="def-btn-success main_color")
+    WebElement Confirm;
+
+    public TransferBetweenUsersPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
 
 
-
+    public void TransferBetweenUsers(){
+        Transfers.click();
+        TransferBetweenUsers.click();
+        DebitForm.click();
+        Card.click();
+        UserName.sendKeys("jsmith");
+        Account.sendKeys("EBQ24123487654");
+        Amount.sendKeys("1000");
+        Continue.click();
+        Confirm.click();
+    }
 }
