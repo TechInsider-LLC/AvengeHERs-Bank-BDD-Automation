e2069a8759a9d30b4a26c5d173307b98f680bb71
NargizaD <128551128+NargizaD@users.noreply.github.com>
2023-05-10 00:11:02 -0400

    API

--- src/test/java/stepDefinitions/OutgoingWireTransfer.java
+++ src/test/java/stepDefinitions/OutgoingWireTransfer.java
@@ -3,6 +3,8 @@ package stepDefinitions;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -15,11 +17,12 @@ import utility.ScenarioData;
 
 import java.time.Duration;
 
+import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
 public class OutgoingWireTransfer {
 
-    WebDriver driver= Hooks.getDriver();
+    WebDriver driver = Hooks.getDriver();
     LogInPage logIn = new LogInPage(driver);
     OutgoingWireTransferPage transfer = new OutgoingWireTransferPage(driver);
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
@@ -28,18 +31,58 @@ public class OutgoingWireTransfer {
     @When("User initiates an outgoing wire transfer")
     public void user_initiates_an_outgoing_wire_transfer() throws InterruptedException {
         logIn.openHomePage();
-        logIn.with(ScenarioData.get("username"),ScenarioData.get("password"));
+        logIn.with(ScenarioData.get("username"), ScenarioData.get("password"));
         Thread.sleep(2000);
         transfer.OutgoingWireTransfer();
 
     }
+
     @Then("Customer should be successfully outgoing wire transfer")
-    public void customer_should_be_successfully_outgoing_wire_transfer(){
+    public void customer_should_be_successfully_outgoing_wire_transfer() {
         String expected = "Back to transfers";
         WebElement message = driver.findElement(By.cssSelector(".text_color-main_color"));
         wait.until(ExpectedConditions.textToBePresentInElement(message, expected));
         String actual = message.getText();
         assertEquals(expected, actual);
 
+
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"data\": {\n" +
+                        "        \"email\": \"Bank-Admin\",\n" +
+                        "        \"password\": \"Demo-Access1\"\n" +
+                        "    }\n" +
+                        "}")
+        .when()
+                .log().all()
+                .post("https://api-demo.ebanq.com/users/public/v1/auth/signin")
+
+        .then()
+                .log().all()
+                .statusCode(200);
+
+
+        Integer Data = given()
+                .contentType(ContentType.JSON)
+
+        .when()
+                .log().all()
+                .post("https://api-demo.ebanq.com/accounts/private/v1/admin/requests/execute/69")
+
+        .then()
+                .log().all()
+                .statusCode(200).extract().response().path("data.accessToken");
+
+        System.out.println("executed");
+
+
+
+
+
+
+
+
+
+        }
     }
-}
