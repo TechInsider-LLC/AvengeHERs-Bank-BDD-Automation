c6de8187d324dc2bc411659a03f95d771918a55f
racoonbest <91798907+racoonbest@users.noreply.github.com>
2023-06-04 01:48:12 -0400

    Update TestDataLoaderMongoDB.java

--- src/test/java/utility/TestDataLoaderMongoDB.java
+++ src/test/java/utility/TestDataLoaderMongoDB.java
@@ -11,7 +11,7 @@ public class TestDataLoaderMongoDB {
     public static final Map<String, Map<String,String>> testData;
 
     static {
-        MongoClient mongoClient = MongoClients.create("mongodb://mongoadmin:secretmongo@0.tcp.ngrok.io:12760/");
+        MongoClient mongoClient = MongoClients.create("mongodb://testdatauser:password@7.tcp.ngrok.io:20788/test-data");
         MongoDatabase database = mongoClient.getDatabase("test-data");
         MongoCollection<Document> collection = database.getCollection("stage-test-data");
 
